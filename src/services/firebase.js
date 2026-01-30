37176a2f60b5dbc6fcbcfd4afc9d4da08c2528bd
Your Name <you@example.com>
2026-01-30 23:57:31 +0530

    Final config fix

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,9 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// This log will help us see if the key is missing in the browser console
-console.log("Checking API Key exists:", !!import.meta.env.VITE_FIREBASE_API_KEY);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
